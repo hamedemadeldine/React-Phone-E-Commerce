536bfb1a9952ab2889c67d2b8814f86606f05a46
hamed emad <hamadaemadeldine@gmail.com>
2020-07-10 16:32:03 +0200

    Updates

---
+++ precache-manifest.e2024e9951cd2c39d8ae9dab8ac05459.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f957030cd6ae659aa2f9936d4cf95816",
+    "url": "/React_p/index.html"
+  },
+  {
+    "revision": "b57d218b7d2b668b03bf",
+    "url": "/React_p/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "535228dfc86904069c3f",
+    "url": "/React_p/static/css/main.b8317dfd.chunk.css"
+  },
+  {
+    "revision": "b57d218b7d2b668b03bf",
+    "url": "/React_p/static/js/2.c7d29477.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/React_p/static/js/2.c7d29477.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "535228dfc86904069c3f",
+    "url": "/React_p/static/js/main.dbe155d7.chunk.js"
+  },
+  {
+    "revision": "4e18377b05727ea9ad8f",
+    "url": "/React_p/static/js/runtime-main.e14503b7.js"
+  },
+  {
+    "revision": "9f7e506d7455da27ce24e474c84a8e0a",
+    "url": "/React_p/static/media/logo.9f7e506d.svg"
+  }
+]);
