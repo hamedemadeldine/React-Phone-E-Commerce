75ed8f0fa13f7786be88eead92bf2f6ba55b1236
unknown <hamadaemadeldine@gmail.comgit config --global user.email hamadaemadeldine@gmail.comgit config --global user.email hamadaemadeldine@gmail.com>
2020-07-10 16:21:40 +0200

    first commit

--- src/component/Cart/CartTotals.js
+++ src/component/Cart/CartTotals.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
 
-export default function CartTotals({ value }) {
+
+export default function CartTotals({ value, history }) {
     const { cartSubTotal, cartTax, cartTotal, clearCart } = value;
     return (
         <React.Fragment>
